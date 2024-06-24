9df7ac343612f0abb4defbf38647aefc0a1ae017
usernameIV <researches.dev.pro@gmail.com>
2024-06-24 17:04:39 -0300

    Abstraction main structure classes

---
+++ src/Bank.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Bank {
+
+    private String name;
+    private List<Account> accounts;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
